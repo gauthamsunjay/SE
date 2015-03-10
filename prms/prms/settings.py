f509057a55166abaada116bec236db470c539b5d
Gautham <gauthamsunjay17@gmail.com>
2015-03-10 17:30:32 +0530

    student app

--- prms/prms/settings.py
+++ prms/prms/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'student'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -85,3 +86,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+if DEBUG:
+    STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),STATIC_URL,"static-only")
